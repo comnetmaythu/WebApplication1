fb9c7f8d9ffb080d0d962a10c9f1678907e94696
cnsa016 <cnsa016@DESKTOP-TEEI30M>
2022-06-03 15:33:38 +0630

    aaa

--- WebApplication1/Default.aspx.cs
+++ WebApplication1/Default.aspx.cs
@@ -11,7 +11,7 @@ namespace WebApplication1
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //20220603 maythu test
+            //20220603 maythu testing
         }
     }
 }
